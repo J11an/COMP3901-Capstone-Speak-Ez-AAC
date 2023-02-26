d07dde739d78ee08124065354f4e09de29d88cd9
Tanya-Lee Lawrence <57540241+Tanzi876@users.noreply.github.com>
2023-02-26 11:35:55 -0500

    Typing required package added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from .config import Config
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+#TypingScreen
+keyboard=CORS(app)
 
 from app import views
