258d61633bfd5187ac73344f2cba4521c189caab
Tanya-Lee Lawrence <57540241+Tanzi876@users.noreply.github.com>
2023-04-04 19:45:26 -0400

    Update seed vocab list.py

--- app/Scripts/seed vocab list.py
+++ app/Scripts/seed vocab list.py
@@ -3,6 +3,9 @@ from openpyxl import load_workbook
 from app import db
 
 def seeder():
+# Take all the rows in all sheet in the Vocab List and make tile model for each row
+
+
   #Load the excel file
   workbook=load_workbook(filename='')
   
@@ -13,4 +16,11 @@ def seeder():
     #loop rows in the sheet
     for row in sheet.iter_rows(min_row=2):
       #create a title model for each row
+      tile=Tile(word=row[0].value,definition=row[1].value)
+      
+      #add the title model to the database
+      db.session.add(title)
+      
+  #commit the chnages to the database
+  db.session.commit()
       
