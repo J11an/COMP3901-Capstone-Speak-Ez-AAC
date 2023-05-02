538ba843fc20f93919d18607ff3e682e22dd5de3
gabster_tech <gabbzbr@outlook.com>
2023-05-02 17:32:29 -0500

    update message bar component

--- src/components/WordPictureTile.vue
+++ src/components/WordPictureTile.vue
@@ -11,26 +11,32 @@ export default {
 </script>
 
 <template>
-  <div class="card aac-card mx-auto" style="width: 18rem">
+  <!--<div class="card aac-card mx-auto" style="width: 18rem">
     <img class="card-img-top img-orientation" :src="imageUrl" alt="" />
     <p class="card-text tile-text">{{ word }}</p>
+  </div>-->
+  <div class="container">
+    <div class="card">
+      <img src="/HelpIcon.png" alt="Image">
+      <div class="card-content">
+        <p>word</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-.img-orientation {
-  /*position: relative;*/
-  width: 100px;
-  height: 100px;
-  top: 10px;
-  left: 90px;
-  justify-content: center;
+
+/*.img-orientation {
+  width: 50px;
+  height: 50px;
+  justify-self: center;
 }
 
 .tile-text {
   font-family: "Thabit";
   font-style: normal;
-  font-weight: 600;
+  font-weight: bold;
   font-size: 32px;
   line-height: 100px;
   text-align: center;
@@ -46,13 +52,44 @@ export default {
   border: 5px solid #000000;
   box-shadow: 0px 2px 6px 2px rgba(0, 0, 0, 0.15);
   border-radius: 48px;
-  width: 150px;
-  height: 272.67px;
+  min-width: 10px;
+  min-height: 100px;
   margin: 10px;
 }
 
 .aac-card:hover {
   transform: scale(1.01);
   transition: 100ms;
+}*/
+
+.card {
+  width: 100px;
+  height: 100px;
+  background: linear-gradient(
+      0deg,
+      rgba(103, 80, 164, 0.08),
+      rgba(103, 80, 164, 0.08)
+    ),
+    #fffbfe;
+  border: 3px solid #000000;
+  box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+  overflow: hidden;
+  justify-self: center;
+  align-items: center;
 }
+
+.card img {
+  display: block;
+  width: 70%;
+  height: 40%;
+  object-fit: contain;
+  margin: 3px;
+}
+
+.card-content {
+  padding: 10px;
+  text-align: center;
+}
+
 </style>
