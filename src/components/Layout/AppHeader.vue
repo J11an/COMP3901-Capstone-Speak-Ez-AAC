6a2eaf1eeee79690626e6443af57734361b88498
Kieran Jaggernauth <thejugg63@gmail.com>
2023-05-16 11:53:15 -0500

    Updated header image sizing

--- src/components/Layout/AppHeader.vue
+++ src/components/Layout/AppHeader.vue
@@ -75,8 +75,12 @@ export default {
   }
 }
 .nav-img:hover{
-  width: 40px;
-  height: 40px;
+  width: 60px;
+  height: 60px;
+  @media (max-width: 600px) {
+    width: 35px;
+    height: 35px
+  }
 }
 
 .nav-link {
